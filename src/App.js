f0ce2d69091f8f7647b41c14a01a9dced798b358
wicopretorius <wico.pretorius@gmail.com>
2023-11-03 18:27:31 +0200

    add WeatherView component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./App.css";
 import { fetchWeather } from "./api/fetchWeather";
+import WeatherView from "./components/WeatherView";
 
 function App() {
   const [query, setQuery] = useState("");
@@ -28,22 +29,8 @@ function App() {
         onKeyDown={search}
       />
       {weather.main && (
-        <div className="city">
-          <h2 className="city-name">
-            <span>{weather.name}</span>
-            <sup>{weather.sys.country}</sup>
-          </h2>
-          <div className="city-temp">
-            {Math.round(weather.main.temp)}
-            <sup>&deg;C</sup>
-          </div>
-          <div className="info">
-            <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description}/>
-            <p>{weather.weather[0].description}</p>
-            
-          </div>
-        </div>
-      )}
+        <WeatherView weather={weather}/>
+       )} 
     </div>
   );
 }
